f443d76a2ef9da6282fd25e7d32a9544523fed11
Jeremy <JeremyWhittier@users.noreply.github.com>
2019-04-25 02:24:46 -0700

    Update server.R

--- server.R
+++ server.R
@@ -35,11 +35,23 @@ function(input, output, session) {
         getGrape(input$city)
       })
     })
-  })  
+  })
+  
+  terms4 <- reactive({
+    # Change when the "update" button is pressed...
+    input$update
+    # ...but not for anything else
+    isolate({
+      withProgress({
+        setProgress(message = "Show me a histogram")
+        getPickle(input$city)
+      })
+    })
+  }) 
   
   output$plot1 <- renderPlot({
     v <- terms()
-    wordcloud(names(v), v, scale=c(4,0.5),
+    wordcloud(names(v), v, scale=c(5,0.5),
                   min.freq = input$freq, max.words=input$max,
                   colors=brewer.pal(8, "Dark2"))
   })
@@ -49,24 +61,39 @@ function(input, output, session) {
 
     comparison.cloud(b, random.order=FALSE,
                      colors = c("#00B2FF", "red", "#FF0099", "#6600CC", "green", "orange", "blue", "brown"),
-                     title.size=1, max.words=input$max, scale=c(2.5, 0.4),rot.per=0.4)
+                     title.size=2, max.words=input$max, scale=c(5, 0.5),rot.per=0.4)
   })
 
   output$plot3 <- renderText({
     value = terms3()
+    value = round(value,digits = 2)
     if (value > 1) {
-      print(paste0('The weather is looking great!', value))
+      print(paste0('Everyone is loving this weather! Score: ', value))
     } else if (value < -1){
-      print(paste0('Be careful outside, weather potentially unsafe ', value))
-    } else if (value > 0 & value < 1){
-      print(paste0('The weather is good ', value))
+      print(paste0('Weather potentially unsafe! Score: ', value))
+    } else if (value > 0 & value < .5){
+      print(paste0('People are not real happy about the weather. Score: ', value))
     } else if (value < 0 & value > -1){
-      print(paste0('The weather is poor, be cautious ', value))
-    } else {
+      print(paste0('The weather is bad, be cautious. Score: ', value))
+    } else if (value > 0.5 & value < 1){
+      print(paste0('Nice weather today! Score: ', value))
+    }
+    else {
       print("Something has happened to earth :(")
     }
     
   })
+  
+  output$plot4 <- renderPlot({
+    df = terms4()
+    
+    ggplot(df, aes(x=syuzhet_emotion.df)) + 
+      geom_histogram(aes(y=..density..), colour="black", fill="white", bins = 15)+
+      geom_density(alpha=.2, fill="#64c651")
+
+    
+  })
+  
   
   
   }
